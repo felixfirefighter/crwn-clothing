b04fe370abb6a51ed19aa7af083b5d6b3d7d194b
Yoong Ti Lee <leeyoongti@gmail.com>
2020-01-19 01:06:09 +0800

    update menu item to navigate with router

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -44,8 +44,8 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ id, imageUrl, size, title }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...props }) => (
+          <MenuItem key={id} {...props} />
         ))}
       </div>
     )
