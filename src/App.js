fc558984797f101cf6eb2701f9928c1a6a099085
Yoong Ti Lee <leeyoongti@gmail.com>
2020-01-19 14:35:22 +0800

    add sign in input and button

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { Route, Switch } from "react-router-dom"
 import "./App.css"
 
 import Header from "./components/header/header.component"
-import HomePage from "./pages/homepage/homepage.component"
-import ShopPage from "./pages/shop/shop.component"
+import HomePage from "./pages/home/home.page"
+import ShopPage from "./pages/shop/shop.page"
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.page"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   )
