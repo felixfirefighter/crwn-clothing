6a8ab2c2295352c5b7dac54132971f3199dc99cb
Yoong Ti Lee <leeyoongti@gmail.com>
2020-02-06 00:28:32 +0800

    change styling to use styled components

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,28 +1,38 @@
 import React from "react"
-import { connect } from 'react-redux'
+import { connect } from "react-redux"
 
-import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions'
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem
+} from "../../redux/cart/cart.actions"
 
 import "./checkout-item.styles.scss"
 
-const CheckoutItem = ({ cartItem , clearItem, addItem, removeItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { imageUrl, name, quantity, price } = cartItem
 
   return (
     <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item" />
-    </div>
-    <span className="name">{name}</span>
-    <span className="quantity">
-      <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
-      <span className="value">{quantity}</span>
-      <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
-
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
       </span>
-    <span className="price">{price}</span>
-    <div className="remove-button" onClick={() => clearItem(cartItem)} >&#10005;</div>
-  </div>)
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
+    </div>
+  )
 }
 
 const mapDispatchToProps = dispatch => ({
--- src/pages/shop/shop.page.jsx
+++ src/pages/shop/shop.page.jsx
@@ -1,35 +1,54 @@
 import React, { Component } from "react"
 
 import { Route } from "react-router-dom"
-import { connect } from 'react-redux'
+import { connect } from "react-redux"
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component"
 import CollectionPage from "../collection/collection.page"
 
-import { updateCollections } from '../../redux/shop/shop.actions'
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
+import WithSpinner from "../../components/with-spinner/with-spinner.component"
+
+import { updateCollections } from "../../redux/shop/shop.actions"
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from "../../firebase/firebase.utils"
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 
 class ShopPage extends Component {
+  state = {
+    loading: true
+  }
+
   unsubscribeFromSnapshot = null
 
   componentDidMount() {
     const { updateCollections } = this.props
-    const collectionRef = firestore.collection('collections')
+    const collectionRef = firestore.collection("collections")
     this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
       updateCollections(collectionsMap)
+      this.setState({ loading: false })
     })
   }
 
   render() {
     const { match } = this.props
-
-    return (
+    const { loading } = this.state
+    return (  
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={props => (<CollectionPageWithSpinner isLoading={loading} {...props} />)}
         />
       </div>
     )
@@ -37,7 +56,7 @@ class ShopPage extends Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  updateCollections: collectionsMap => dispatch(updateCollections())
+  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
 })
 
 export default connect(null, mapDispatchToProps)(ShopPage)
