9affbd5f90720fe947db9b1f17127136c4004ee0
Yoong Ti Lee <leeyoongti@gmail.com>
2020-02-05 15:34:13 +0800

    add code to push collections to firebase

--- src/App.js
+++ src/App.js
@@ -11,15 +11,16 @@ import HomePage from "./pages/home/home.page"
 import ShopPage from "./pages/shop/shop.page"
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.page"
 
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
+import { auth, createUserProfileDocument, addCollectionAndDocuments } from "./firebase/firebase.utils"
 import { setCurrentUser } from "./redux/user/user.actions"
 import { selectCurrentUser } from './redux/user/user.selectors'
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors'
 
 class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const { setCurrentUser } = this.props
+    const { setCurrentUser, collectionsArray } = this.props
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -33,6 +34,7 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth)
+      addCollectionAndDocuments('collections', collectionsArray.map(({ title, items }) => ({ title, items })))
     })
   }
 
@@ -58,7 +60,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview
 })
 
 const mapDispatchToProps = dispatch => ({
