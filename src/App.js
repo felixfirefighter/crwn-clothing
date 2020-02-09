a73935a22f7df509b3263778a9854afa69409800
Yoong Ti Lee <leeyoongti@gmail.com>
2020-02-09 17:30:25 +0800

    migrate existing auth to use redux saga

--- src/App.js
+++ src/App.js
@@ -11,33 +11,14 @@ import HomePage from "./pages/home/home.page"
 import ShopPage from "./pages/shop/shop.page"
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.page"
 
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
-import { setCurrentUser } from "./redux/user/user.actions"
 import { selectCurrentUser } from './redux/user/user.selectors'
+import { checkUserSession } from './redux/user/user.actions'
 
 class App extends React.Component {
-  unsubscribeFromAuth = null
 
   componentDidMount() {
-    const { setCurrentUser } = this.props
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth)
-        userRef.onSnapshot(snapshot => {
-          setCurrentUser({
-            id: snapshot.id,
-            ...snapshot.data()
-          })
-        })
-      }
-
-      setCurrentUser(userAuth)
-    })
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth()
+    const { checkUserSession } = this.props
+    checkUserSession()
   }
 
   render() {
@@ -62,7 +43,7 @@ const mapStateToProps = createStructuredSelector({
 })
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+  checkUserSession: () => dispatch(checkUserSession())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
